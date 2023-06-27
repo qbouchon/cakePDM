6dd78ae7d177b32b2e5c063019138b9c7baceeba
P'tit Bouchon <quentin.bouchon.pro@gmail.com>
2023-06-27 16:37:15 +0200

    sideNav +inclue jquery

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -8,10 +8,10 @@
     </title>
     <?= $this->Html->meta('icon') ?>
 
-    <!-- <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet"> -->
+    <!-- jquery -->
+    <?= $this->Html->script('../Jquery/js/jquery.min'); ?>
 
-    <!-- On utilise Bootstrap, décommenter, ajouter echo pour utiliser le css de cake (casse le visu)-->
-    <!--  $this->Html->css(['normalize.min', 'milligram.min', 'cake']) ?> -->
+    <!-- Bootstrap -->
     <?= $this->Html->css('BootstrapUI.bootstrap.min'); ?>
     <?= $this->Html->css(['BootstrapUI./font/bootstrap-icons', 'BootstrapUI./font/bootstrap-icon-sizes']); ?>
     <?= $this->Html->script(['BootstrapUI.popper.min', 'BootstrapUI.bootstrap.min']); ?>
@@ -20,14 +20,13 @@
     <?= $this->fetch('css') ?>
     <?= $this->fetch('script') ?>
     
-
     <!-- Css et Js pour le menu de gauche -->
     <?= $this->Html->script('sideNav'); ?>
     <?= $this->Html->css('sideNav'); ?>
 
-    <!-- fontAwesome -->
-    <?= $this->Html->script('Fontawesome./js/all'); ?>
-    <?= $this->Html->css('Fontawesome./css/all'); ?>
+    <!-- fontAwesome (trouver une manière plus propre ?) -->
+    <?= $this->Html->script('../Fontawesome/js/all'); ?>
+    <?= $this->Html->css('../Fontawesome./css/all'); ?>
 
 </head>
 <body>
@@ -37,9 +36,9 @@
             
             <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
                 <!-- Sidebar Toggle-->
-                <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+                <button class="btn btn-link btn-sm order-1 order-lg-0 ms-2 me-4 me-lg-0 text-white" id="sidebarToggle" href="#!"><i class="flex icon fas fa-bars fa-2x "></i></button>
                 <!-- Navbar Brand-->
-                <a class="navbar-brand ps-3" href="">CakePDM</a>
+                <a class="ps-2 h3 text-white text-decoration-none " href="">CakePDM</a>
                 <!-- Navbar Search-->
                 <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
                     <div class="input-group">
@@ -70,57 +69,75 @@
                 </ul> 
             </nav>
             
+
             <!-- side menu -->
             <div id="layoutSidenav">
                 <div id="layoutSidenav_nav">
                     <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
                         <div class="sb-sidenav-menu">
                             <div class="nav">
-                               <!-- {# Pour ajouter un sous menu #} -->
-                                <div class="sb-sidenav-menu-heading">Menu</div>
+
+                               <!-- Sous menu -->
+                                <div class="text-white sb-sidenav-menu-heading">Menu</div>
+
+
                                     <a class="nav-link" href="">
                                         <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                         Réservation
                                     </a>
 
-                                            <div class="sb-sidenav-menu-heading">Administration</div>
+                                <!-- Sous menu -->
+                                <div class=" text-white sb-sidenav-menu-heading">Administration</div>
 
-                                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePrets" aria-expanded="false" aria-controls="collapsePrets">
-                                             <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                             Réservations
-                                             <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                            </a>
-                                            <div class="collapse" id="collapsePrets" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                                                <nav class="sb-sidenav-menu-nested nav">
-                                                    <a class="nav-link" href="">Nouvelle réservation</a>
-                                                    <a class="nav-link" href="">Liste des Réservations</a>
-                                                </nav>
-                                            </div>
-                                            
-                                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseMatos" aria-expanded="false" aria-controls="collapseMatos">
-                                             <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                             Matos
-                                             <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                            </a>
-                                            <div class="collapse" id="collapseMatos" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                                                <nav class="sb-sidenav-menu-nested nav">
-                                                    <a class="nav-link" href="">Nouveau Matos</a>
-                                                    <a class="nav-link" href="">Liste des Matos</a>
-                                                    
-                                                </nav>
+                                            <div class="collapseOver">
+                                                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseReservations" aria-expanded="false" aria-controls="collapseReservations">
+                                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                                 <div class="nav-item">Réservations</div>
+                                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                                                </a>
+                                                <div class="collapse" id="collapseReservations" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                                                    <nav class="sb-sidenav-menu-nested nav">
+                                                        <!-- <a class="nav-link" href="">Liste des Réservations</a>
+                                                        <a class="nav-link" href="">Créer une Réservation</a> -->
+                                                        <?= $this->Html->link(__('Liste des Réservations'), ['controller'=>'Reservations','action' => 'index'], ['class' => 'nav-link']) ?>
+                                                        <?= $this->Html->link(__('Créer une Réservation'), ['controller'=>'Reservations','action' => 'add'], ['class' => 'nav-link']) ?>
+                                                    </nav>
+                                                </div>
                                             </div>
 
+                                            <div class="collapseOver">
+                                                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseRessources" aria-expanded="false" aria-controls="collapseRessources">
+                                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                                 Ressources & Domaines
+                                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                                                </a>
+                                                <div class="collapse" id="collapseRessources" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                                                    <nav class="sb-sidenav-menu-nested nav">
+                                                       <!--  <a class="nav-link" href="">Liste des Ressources</a>
+                                                        <a class="nav-link" href="">Créer une Ressource</a>
+                                                        <a class="nav-link" href="">Liste des Domaines</a>
+                                                        <a class="nav-link" href="">Créer un Domaine</a> -->
+                                                        <?= $this->Html->link(__('Liste des Ressources'), ['controller'=>'Resources','action' => 'index'], ['class' => 'nav-link']) ?>
+                                                        <?= $this->Html->link(__('Créer une Ressource'), ['controller'=>'Resources','action' => 'add'], ['class' => 'nav-link']) ?>
+                                                        <?= $this->Html->link(__('Liste des Domaines'), ['controller'=>'Domains','action' => 'index'], ['class' => 'nav-link']) ?>
+                                                        <?= $this->Html->link(__('Créer un Domaine'), ['controller'=>'Domains','action' => 'add'], ['class' => 'nav-link']) ?>
+                                                        
+                                                    </nav>
+                                                </div>
+                                            </div>
 
-                                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseUtilisateurs" aria-expanded="false" aria-controls="collapseUtilisateurs">
-                                             <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                             Utilisateurs
-                                             <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                            </a>
-                                            <div class="collapse" id="collapseUtilisateurs" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                                                <nav class="sb-sidenav-menu-nested nav">
-                                                    <a class="nav-link" href="">Nouvel Utilisateur</a>
-                                                    <a class="nav-link" href="">Liste des Utilisateurs</a>
-                                                </nav>
+                                            <div class="collapseOver">
+                                                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseUtilisateurs" aria-expanded="false" aria-controls="collapseUtilisateurs">
+                                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                                 Utilisateurs
+                                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                                                </a>
+                                                <div class="collapse" id="collapseUtilisateurs" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                                                    <nav class="sb-sidenav-menu-nested nav">
+                                                        <!-- <a class="nav-link" href="">Liste des Utilisateurs</a> -->
+                                                        <?= $this->Html->link(__('Liste des Utilisateurs'), ['controller'=>'Users','action' => 'index'], ['class' => 'nav-link']) ?>
+                                                    </nav>
+                                                </div>
                                             </div>
                             </div>
                         </div>
--- webroot/js/sideNav.js
+++ webroot/js/sideNav.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', event => {
 
     // Toggle the side navigation
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
+
     if (sidebarToggle) {
         // Uncomment Below to persist sidebar toggle between refreshes
-        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-        //     document.body.classList.toggle('sb-sidenav-toggled');
-        // }
+        //if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+         //  document.body.classList.toggle('sb-sidenav-toggled');
+         //}
         sidebarToggle.addEventListener('click', event => {
             event.preventDefault();
             document.body.classList.toggle('sb-sidenav-toggled');
@@ -14,4 +15,69 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     }
 
+    // //mouseOver on nav-item
+    // document.querySelectorAll('.collapseOver').forEach(function(item){
+
+    //     item.addEventListener('mouseenter', function(e){
+
+
+    //         let id_target = this.querySelector('.nav-link').getAttribute('data-bs-target');
+    //         let target = document.querySelector(id_target);
+    //         if(target != null){
+    //              //console.log(id_target + "Collapsing" +  target.classList.contains('collapsing'))
+    //              console.log(id_target + "show" + target.classList.contains('show'))
+    //             if(!target.classList.contains('collapsing') && !target.classList.contains('show'))
+    //             {
+    //                //target.setAttribute('class','collaping');
+    //                new bootstrap.Collapse(target); 
+    //             }
+             
+
+    //         }
+    //     })
+
+    //     item.addEventListener('mouseleave', function(e){
+
+    //         let id_target = this.querySelector('.nav-link').getAttribute('data-bs-target');
+    //         let target = document.querySelector(id_target);
+    //         if(target != null){
+    //             // if(!target.classList.contains('collapsing') && target.classList.contains('show'))
+    //             // {
+    //                 target.setAttribute('class','collapse');
+    //                 //new bootstrap.Collapse(target);
+
+    //             //}
+
+    //         }
+    //     })
+
+    // });
+
+    
+
+});
+
+
+//Pour garder la même config du menu au rechargement d'une page
+$(document).ready(function () {
+
+    $(".collapse").on("shown.bs.collapse", function () {
+        localStorage.setItem("coll_" + this.id, true);
+    });
+
+    $(".collapse").on("hidden.bs.collapse", function () {
+        localStorage.removeItem("coll_" + this.id);
+    });
+    
+    $(".collapse").each(function () {
+        if (localStorage.getItem("coll_" + this.id) === "true") {
+            $(this).toggleClass('no-transition');
+            $(this).collapse('show');
+            $(this).toggleClass('no-transition');
+        }
+        // else {
+        //     $(this).collapse("hide");
+        // }
+
+    });
 });
