544a1b99d9d2778681eccc65a5357955ec16241a
P'tit Bouchon <quentin.bouchon.pro@gmail.com>
2023-09-07 16:34:21 +0200

    closing date ok on reservation

--- templates/ClosingDates/edit.php
+++ templates/ClosingDates/edit.php
@@ -5,18 +5,7 @@
  */
 ?>
 <div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Form->postLink(
-                __('Delete'),
-                ['action' => 'delete', $closingDate->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $closingDate->id), 'class' => 'side-nav-item']
-            ) ?>
-            <?= $this->Html->link(__('List Closing Dates'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column-responsive column-80">
+
         <div class="closingDates form content">
             <?= $this->Form->create($closingDate) ?>
             <fieldset>
