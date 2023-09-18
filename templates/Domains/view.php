be4a55a470b5f08e6188be2c63f7c3b2a09c4b68
P'tit Bouchon <quentin.bouchon.pro@gmail.com>
2023-09-18 08:42:14 +0200

    domain view/rsources actions

--- templates/Domains/view.php
+++ templates/Domains/view.php
@@ -37,15 +37,65 @@
                                 <th><?= __('Archive') ?></th>
                                 <th class="actions"><?= __('Actions') ?></th>
                             </tr>
-                            <?php foreach ($domain->resources as $resources) : ?>
+                            <?php foreach ($domain->resources as $resource) : ?>
                                 <tr class="bg-white">
-                                    <td><?= h($resources->name) ?></td>
-                                    <td><?= $resources->description ?></td>
-                                    <td><?= $resources->archive ? 'Oui' : 'Non' ?></td>
-                                    <td class="actions">
-                                        <?= $this->Html->link(__('View'), ['controller' => 'Resources', 'action' => 'view', $resources->id]) ?>
+                                    <td><?= h($resource->name) ?></td>
+                                    <td><?= $resource->description ?></td>
+                                    <td><?= $resource->archive ? 'Oui' : 'Non' ?></td>
+
+
+                                     <td class="actions d-flex justify-content-center">
+                                        <div class="dropdown">
+                                            <button  class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+                                                <?=__('Actions') ?>
+                                            </button>
+                                            <ul class="dropdown-menu">
+                                                <li><?= $this->Html->link(__('View'), ['controller' => 'Resources', 'action' => 'view', $resource->id],['class' => 'dropdown-item']) ?></li>
+                                                
+                                                <li><?= $this->Html->link(__('Edit'), ['controller' => 'Resources', 'action' => 'edit', $resource->id],['class' => 'dropdown-item']) ?></li>
+                                                <li>
+                                                   <?= $resource->archive ? $this->Form->postLink(__('Désarchiver'), ['controller' => 'Resources', 'action' => 'unArchive', $resource->id],['class' => 'dropdown-item']) : $this->Form->postLink(__('Archiver'), ['controller' => 'Resources', 'action' => 'archive', $resource->id],['class' => 'dropdown-item']) ?>
+                                                </li>
+                                                <!-- Button trigger DeleteResourceModal -->
+                                                <li>
+                                                    <button type="button" class="btn btn-danger text-danger dropdown-item" data-bs-toggle="modal" data-bs-target="<?= '#deleteResourceModal' . $resource->id ?>">
+                                                      <?= __('Supprimer'); ?>
+                                                    </button>
+                                                </li>
+                                            </ul>
+                                        </div>
                                     </td>
                                 </tr>
+
+
+                                <!-- DeleteResourceModal -->
+                                <div class="modal fade" id="<?= 'deleteResourceModal' . $resource->id ?>" tabindex="-1" aria-labelledby="deleteResourceModalLabel" aria-hidden="true">
+                                  <div class="modal-dialog">
+                                    <div class="modal-content">
+                                      <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="deleteResourceModalLabel"><?= 'Suppression de la ressource ' . $resource->name ?> </h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                      </div>
+                                      <div class="modal-body">
+                                        Attention, si vous supprimez cette ressource, <b>toutes les réservations associées et fichiers seront définitivement supprimés.</b> Si vous souhaitez que cette ressource ne soit plus disponible mais garder les anciennes réservations et fichiers, considérez  d'utiliser l'option "archiver" à la place.
+                                      </div>
+                                      <div class="modal-footer">  
+                                        <?= $this->Form->postLink(__('Supprimer'), ['controller' => 'Resources','action' => 'delete', $resource->id], ['class' => 'btn btn-danger', 'confirm' => 'Supprimer '.$resource->name.' ?']) ?>    
+
+                                        <?php
+                                            if($resource->archive)
+                                                echo $this->Form->postLink(__('Désarchiver'), ['controller' => 'Resources','action' => 'unArchive', $resource->id], ['class' => 'btn btn-warning', $resource->id]);
+                                            else
+                                                echo $this->Form->postLink(__('Archiver'), ['controller' => 'Resources','action' => 'archive', $resource->id], ['class' => 'btn btn-warning', $resource->id]);
+                                        ?>
+
+                                       
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+
                             <?php endforeach; ?>
                         </table>
                     </div>
