f25e7b341ba9bc0b98eaef1193541a6dfcba2653
P'tit Bouchon <quentin.bouchon.pro@gmail.com>
2023-10-09 10:04:30 +0200

    User menu collapse Resa

--- templates/element/user_menu.php
+++ templates/element/user_menu.php
@@ -3,4 +3,15 @@
 
 <?= $this->Html->link('<div class="sb-nav-link-icon"><i class="fa-solid fa-house"></i></div>Accueil', ['controller'=>'pages','action' => 'display'], ['class' => 'nav-link', 'escape' => false]) ?>
 <?= $this->Html->link('<div class="sb-nav-link-icon"><i class="fa-solid fa-book-open"></i></div>Catalogue', ['controller'=>'pages','action' => 'catalogue'], ['class' => 'nav-link', 'escape' => false]) ?>
-<?= $this->Html->link('<div class="sb-nav-link-icon"><i class="fa-regular fa-calendar-check"></i></div>Réservations', ['controller'=>'Reservations','action' => 'indexUser'], ['class' => 'nav-link', 'escape' => false]) ?>
+
+<a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseUserReservations" aria-expanded="false" aria-controls="collapseUserReservations">
+    <div class="sb-nav-link-icon"><i class="fa-regular fa-calendar-check"></i></i></div>
+    <div class="nav-item">Réservations</div>
+    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+</a>
+<div class="collapse" id="collapseUserReservations" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+    <nav class="sb-sidenav-menu-nested nav">       
+        <?= $this->Html->link(__('Mes réservations'), ['controller'=>'Reservations','action' => 'indexUser'], ['class' => 'nav-link']) ?>
+        <?= $this->Html->link(__('Créer une réservation'), ['controller'=>'Reservations','action' => 'add'], ['class' => 'nav-link']) ?>
+    </nav>
+</div>
